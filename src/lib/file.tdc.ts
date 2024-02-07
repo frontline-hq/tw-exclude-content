d5b60b656f1a918b5bacaa7c72ed8a02275532d1
Benjamin Preiss <benjaminpreiss@protonmail.com>
2024-02-07 14:00:52 +0100

    Construct reproducible

---
+++ src/lib/file.tdc.ts
@@ -0,0 +1,1 @@
+export const blue = 'bg-blue-900';
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import plugin from 'tailwindcss/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [plugin(() => {}, { content: ['!**/*.tdc.ts'] })]
+};
